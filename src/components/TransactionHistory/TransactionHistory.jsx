611e34984f0a8734e4ec420df4169a98268e2901
Alexandr-Styopin <79509688+Alexandr-Styopin@users.noreply.github.com>
2024-04-27 10:58:31 +0200

    Transaction_edit

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -2,15 +2,15 @@ import TransactionItem from "./TransactionItem/TransactionItem";
 import css from "../TransactionHistory/TransactionHistory.module.css";
 export default function TransactionHistory({ transactions }) {
   return (
-    <table className={css.tablebody}>
-      <thead>
+    <table className={css.table}>
+      <thead className={css.tableHead}>
         <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+          <th className={css.th}>Type</th>
+          <th className={css.th}>Amount</th>
+          <th className={css.th}>Currency</th>
         </tr>
       </thead>
-      <tbody>
+      <tbody className={css.tbody}>
         {transactions.map(({ id, type, amount, currency }) => (
           <TransactionItem
             key={id}
--- src/components/TransactionHistory/TransactionItem/TransactionItem.jsx
+++ src/components/TransactionHistory/TransactionItem/TransactionItem.jsx
@@ -1,9 +1,11 @@
+import css from "../TransactionItem/TransactionItem.module.css";
+
 export default function TransactionItem({ type, amount, currency }) {
   return (
     <tr>
-      <td>{type}</td>
-      <td>{amount}</td>
-      <td>{currency}</td>
+      <td className={css.td}>{type}</td>
+      <td className={css.td}>{amount}</td>
+      <td className={css.td}>{currency}</td>
     </tr>
   );
 }
